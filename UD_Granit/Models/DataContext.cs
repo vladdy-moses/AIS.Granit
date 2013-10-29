ab4609f129587b764974c0931f46b93559b1b0dc
Vladislav Moiseev <v.moiseev94@gmail.com>
2013-10-29 09:28:40 +0400

    Add DataContext with Users table

---
+++ UD_Granit/Models/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UD_Granit.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection") { }
+
+        public DbSet<User> Users;
+    }
+
+    public class User
+    {
+        [Key]
+        public int User_Id { set; get; }
+
+        [Required]
+        public string Email { set; get; }
+        [Required]
+        public string Password { set; get; }
+
+        [Required]
+        public string FirstName { set; get; }
+        [Required]
+        public string SecondName { set; get; }
+        public string LastName { set; get; }
+    }
+}
