0e729ddedefbb1aeeaca510a204cc28a135de509
Vladislav Moiseev <v.moiseev94@gmail.com>
2013-11-08 15:46:39 +0400

    Add Controller helper

--- UD_Granit/Controllers/HomeController.cs
+++ UD_Granit/Controllers/HomeController.cs
@@ -50,20 +50,8 @@ namespace UD_Granit.Controllers
                 }
             }
             */
-            return View("Index");
-        }
-
-
-        private string GetUserIP()
-        {
-            string ipList = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
-
-            if (!string.IsNullOrEmpty(ipList))
-            {
-                return ipList.Split(',')[0];
-            }
-
-            return Request.ServerVariables["REMOTE_ADDR"];
+            ViewBag.asd = this.GetUserIp();
+            return View();
         }
     }
 }
---
+++ UD_Granit/Views/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UD_Granit.Controllers//.Views
+{
+    public static class Helpers
+    {
+        public static string GetUserIp(this Controller controller)
+        {
+            string ipList = controller.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipList))
+            {
+                return ipList.Split(',')[0];
+            }
+
+            return controller.Request.ServerVariables["REMOTE_ADDR"];
+            //return ;
+        }
+    }
+}
