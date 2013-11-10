b9a60f8543d6c9e4334892b00e85f00a072b0b2e
Vladislav Moiseev <v.moiseev94@gmail.com>
2013-11-10 12:27:37 +0400

    Add account controller

---
+++ UD_Granit/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace UD_Granit.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Account/Login/
+
+        public ActionResult Login()
+        {
+            //FormsAuthentication.
+            return View();
+        }
+    }
+}
