19db44352baf6050d0f33109e4d22f2725d3100e
Vladislav Moiseev <v.moiseev94@gmail.com>
2013-11-21 13:30:58 +0400

    Add required fields fot applicant

--- UD_Granit/Models/Users.cs
+++ UD_Granit/Models/Users.cs
@@ -45,28 +45,43 @@ namespace UD_Granit.Models
 
     public class Applicant : User
     {
+        [Required]
         [Display(Name = "Наименование организации")]
         public string Organization { set; get; }
+
+        [Required]
         [Display(Name = "Подразделение")]
         public string Organization_Depatment { set; get; }
+        
         [Display(Name = "Заключение организации")]
         public string Organization_Conclusion { set; get; }
+        
         [Display(Name = "День рождения")]
         public DateTime? Birthday { set; get; }
-        //[Display(Name = "Электронный адрес")]
+        
+        [Display(Name = "Учёная степень")]
         public bool Ph_D { set; get; }
+
         //Идентификатор руководителя
+
+        [Required]
         [Display(Name = "Город")]
         public string City { set; get; }
+
+        [Required]
         [Display(Name = "Адрес")]
         public string Address { set; get; }
+
+        [Required]
         [Display(Name = "Обучался в аспирантуре?")]
         public bool WasInGraduateSchool { set; get; }
+
         [Display(Name = "Высшее учебное заведение, где проходил аспирантуру")]
         public string University { set; get; }
+
         [Display(Name = "Подразделение, где проходил аспирантуру")]
         public string University_Departmant { set; get; }
-        //[Required]
+
         public Dissertation Dissertation { set; get; }
     }
 
