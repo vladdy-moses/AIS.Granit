ed0463b8b6024389a500c603b441d048bd2d5c3b
Vladislav Moiseev <v.moiseev94@gmail.com>
2013-11-26 09:35:23 +0400

    Fix EnumHelper

---
+++ UD_Granit/Helpers/EnumHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace System.Web.Mvc.Html
+{
+    public static class EnumHelper
+    {
+        public static string ToDescription(this Enum value)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+            var attributes = (DescriptionAttribute[])value.GetType().GetField(
+                Convert.ToString(value)).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : Convert.ToString(value);
+        }
+
+        public static MvcHtmlString EnumDropDownListFor<TModel, TEnum>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TEnum>> expression, object attributes = null)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            IEnumerable<TEnum> values = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+
+            IEnumerable<SelectListItem> items =
+                values.Select(value => new SelectListItem
+                {
+                    Text = (value as Enum).ToDescription(),
+                    Value = value.ToString(),
+                    Selected = value.Equals(metadata.Model)
+                });
+
+            return htmlHelper.DropDownListFor(
+                expression,
+                items
+                );
+        }
+    }
+}
