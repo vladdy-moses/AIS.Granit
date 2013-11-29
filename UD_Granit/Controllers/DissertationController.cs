902d26d9c682ca6bcc87a909e3071e8aee782671
Vladislav Moiseev <v.moiseev94@gmail.com>
2013-11-29 21:26:17 +0400

    Download dissertation files (last commit) and clean

--- UD_Granit/Controllers/DissertationController.cs
+++ UD_Granit/Controllers/DissertationController.cs
@@ -195,19 +195,6 @@ namespace UD_Granit.Controllers
                 return HttpNotFound();
 
             return File("~/App_Data/" + fileName, "binary/octet-stream", fileName);
-            /*Response.Clear();
-            Response.ClearHeaders();
-            Response.ClearContent();
-
-            Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
-            Response.AddHeader("Content-Length", file.Length.ToString());
-
-            Response.ContentType = "text/plain";
-            Response.Flush();
-            Response.TransmitFile(file.FullName);
-            Response.End();
-
-            return HttpNotFound();*/
         }
 
         private bool CanShow(Dissertation dissertation)
